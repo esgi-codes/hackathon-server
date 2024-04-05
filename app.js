06034149915148aa6e349755ec0b60c886a438b2
🦀 adia-dev <abdoudu78130@gmail.com>
2024-04-05 08:37:47 +0200

    Add CORS middleware to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const listEndpoints = require('express-list-endpoints')
@@ -19,6 +20,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
