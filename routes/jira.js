dd462f9a5e2c907b1bd78a548b310ce5f1aa860f
🦀 adia-dev <abdoudu78130@gmail.com>
2024-04-05 08:37:52 +0200

    Update Jira routes to include new endpoints for unassigned and assigned issues

--- routes/jira.js
+++ routes/jira.js
@@ -32,12 +32,57 @@ router.get("/project/:projectId", async (req, res) => {
 
 router.get("/issues", async (req, res) => {
     try {
-        const issuesURL = `${jiraBaseURL}/rest/agile/1.0/board/1/epic/none/issue`;
+        const issuesURL = `${jiraBaseURL}/rest/agile/1.0/board/1/epic/none/issue?expand=names&fields=summary,assignee,status,customfield_10060`;
         const response = await axios.get(issuesURL, axiosConfig);
 
         res.json(response.data.issues);
     } catch (error) {
         console.error("Error fetching issues:", error);
+        res.status(500).send("Failed to fetch issues.");
+    }
+});
+
+router.get("/issues/unassigned", async (req, res) => {
+    try {
+        const issueURL = `${jiraBaseURL}/rest/agile/1.0/board/1/epic/none/issue?jql=assignee=null`;
+        const response = await axios.get(issueURL, axiosConfig);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching issues:", error.message);
+        if (error.response.status === 404) {
+            return res.status(404).send("Assignee not found.");
+        }
+        res.status(500).send("Failed to fetch issues.");
+    }
+});
+
+router.post("/issues/unassign", async (req, res) => {
+    try {
+        const unassignURL = `${jiraBaseURL}/rest/api/3/issue/${req.body.issueId}/assignee`;
+        const response = await axios.put(unassignURL, { name: null }, axiosConfig);
+
+        res.status(204).send();
+    } catch (error) {
+        console.error("Error unassigning issue:", error.message);
+        if (error.response.status === 404) {
+            return res.status(404).send("Issue not found.");
+        }
+        res.status(500).send("Failed to unassign issue.");
+    }
+});
+
+router.get("/issues/assigned/:assignee", async (req, res) => {
+    try {
+        const issueURL = `${jiraBaseURL}/rest/agile/1.0/board/1/epic/none/issue?jql=assignee=${req.params.assignee}`;
+        const response = await axios.get(issueURL, axiosConfig);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching issues:", error.message);
+        if (error.response.status === 404) {
+            return res.status(404).send("Assignee not found.");
+        }
         res.status(500).send("Failed to fetch issues.");
     }
 });
