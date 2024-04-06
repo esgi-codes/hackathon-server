6856e6dd7c99bbf76515fda3f799b30383885d5e
paschyz <paschy2002@gmail.com>
2024-04-06 11:29:47 +0200

    removed duplicate cors

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-const cors = require('cors');
 var path = require('path');
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
@@ -22,7 +21,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
