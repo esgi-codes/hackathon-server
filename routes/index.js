0032b59df25163f487b4e286eb4d63a0354a55c7
🦀 adia-dev <abdoudu78130@gmail.com>
2024-04-06 12:10:58 +0200

    Add health check route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function (req, res, next) {
+  res.send('OK');
 });
 
 module.exports = router;
